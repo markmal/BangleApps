05ce618fd1caf9fd8f45d0b442d1c3618d1f2cba
markmal <markmal@github.com>
2021-11-13 14:26:08 -0500

    Speed display improved

--- apps/wsspeed/wsspeed.app.js
+++ apps/wsspeed/wsspeed.app.js
@@ -2,7 +2,7 @@ Bangle.loadWidgets();
 Bangle.drawWidgets();
 
 var LastFix = {fix:-1,satellites:0};
-var Speed=0.0, Speed2sec=0.0, Speed10sec=0.0;
+var Speed=0.0, Speed2sec=0.0, Speed10sec=0.0, TotalDistance=0.0;
 
 /*
 var Layout = require("Layout");
@@ -73,6 +73,11 @@ function renderDigit(D, x,y, w,h) {
 // previous digits
 var dHp=" ", dDp=" ", dSp="0", dFp="0", Up=" ", dSpeed2sec=NaN, dSpeed10sec=NaN, Lp=0;
 
+function resetDigits(){
+  dHp=" "; dDp=" "; dSp="0"; dFp="0"; Up=" "; 
+  dSpeed2sec=NaN; dSpeed10sec=NaN; Lp=0;
+}
+
 function renderSpeeds(speed, speed2s, speed10s, unit) {
   var S = speed.toFixed(1);
   var L = S.length;
@@ -86,6 +91,11 @@ function renderSpeeds(speed, speed2s, speed10s, unit) {
 
   //print(S, dH,dD,dS,dF);
 
+  g.drawLine(0,25,176,25);
+  g.setFont("6x8",1); 
+  g.clearRect(60, 26, 150, 34);
+  g.drawString(TotalDistance.toFixed(2)+" Km", 60, 26);
+  
   var x0=60, dW=30, y0=35;
   if(speed>=10) x0 = 35;
   if(speed>=100) x0 = 9;
@@ -115,6 +125,7 @@ function renderSpeeds(speed, speed2s, speed10s, unit) {
     g.setFont("Vector",16);
     g.drawString("max 2s",15,105);
     g.setFont("Vector",29);
+    g.clearRect(15,119, 90,148);
     g.drawString(speed2s.toFixed(1),15,119);
     dSpeed2sec=speed2s;
   }
@@ -122,9 +133,11 @@ function renderSpeeds(speed, speed2s, speed10s, unit) {
     g.setFont("Vector",16);
     g.drawString("max 10s",95,105);
     g.setFont("Vector",29);
+    g.clearRect(95,119, 170,148);
     g.drawString(speed10s.toFixed(1),95,119);
     dSpeed10sec=speed10s;
   }
+
   g.drawLine(0,150,176,150);
 }
 
@@ -135,7 +148,8 @@ function addSpeed(time, speed, lat, lon) {
     LastSpeeds.pop();
 }
 
-var MaxSpeed2sec=0, MaxSpeed10sec=0;
+var MaxSpeed2sec=0.0, MaxSpeed10sec=0.0;
+
 function calcSpeed2n10sec() {
   if (LastSpeeds.length>1) {
     var recA = LastSpeeds[0];
@@ -185,23 +199,109 @@ function onGPS(fix) {
   addSpeed(fix.time, fix.speed, fix.lat, fix.lon);
   calcSpeed2n10sec();
   
+  if(LastFix && LastFix.time)
+    TotalDistance = TotalDistance + Speed * (fix.time - LastFix.time)*0.001/3600;
   LastFix = fix;
   LastFix.speed = Speed;
 }
 
+
+var SimSpeed = 20, SimAcceleration = 1.0;
+function simulateGPS() {
+  if( (Date.now() % 60000) < 30000)
+    SimAcceleration = 1.1;
+  else
+    SimAcceleration = 0.9;
+  SimSpeed = SimSpeed * SimAcceleration;
+  onGPS({fix:1,satellites:3,speed:SimSpeed, 
+       time:Date.now()});
+}
+
 function onInterval() {
+  simulateGPS();
   if(IsSpeedDisplayDirty)
     renderSpeeds(Speed, MaxSpeed2sec, MaxSpeed10sec, "kph");
   IsSpeedDisplayDirty = false;
 }
 
+var IsRecording = false;
+var BtnRecording = "Start Rec";
+function toggleRecording(){
+  IsRecording = ! IsRecording;
+  BtnRecording = IsRecording ? "Stop Rec" : "Start Rec";
+  print(BtnRecording);
+  hideMenu();
+}
+
+// GUI
+var Layout = require("Layout");
+
+var loDisplay = {
+  type:"v", c: [
+    {type:"txt", font:"6x8:4", label:"A Test", id:"label"},
+  ]
+};
+var opDisplay = {btns:[
+  {label:"Menu", cb: l=>showMenu()}
+], lazy:true};
+
+var loMenu = {
+  type:"v", c: [
+    //{type:"txt", font:"6x8:2", label:"A Test", id:"label"},
+    {type:"btn", font:"6x8:2", color:"Red", label:BtnRecording, cb: l=>toggleRecording() },
+    {type:"btn", font:"6x8:2", label:"Exit", cb: l=>hideMenu() }
+  ]
+};
+var opMenu = {btns:[{label:"Close",cb: l=>hideMenu()}],
+    lazy:false
+   };
+
+var layout; // = new Layout(loMenu, opMenu);
+var WatchButton;
+var DisplayInterval;
+
+function onWatchButton() {
+    console.log("Pressed");
+    if(!layout)
+      showMenu();
+  }
+
+function showMenu() {
+  print("showMenu");
+  print("clr DisplayInterval:",DisplayInterval);
+  clearInterval(DisplayInterval);
+  clearWatch(WatchButton);
+  //layout.clear();
+  g.clear();
+  layout = new Layout(loMenu, opMenu);
+  layout.render();
+}
+
+function hideMenu() {
+  print("hideMenu");
+  if(!layout) return;
+  if(layout)
+    layout.clear(); 
+  layout = null;
+  //layout = new Layout(loDisplay, opDisplay);
+  //layout.render();
+  resetDigits();
+  WatchButton = setWatch(onWatchButton, BTN, {edge:"falling", debounce:100, repeat:false});
+  DisplayInterval = setInterval(onInterval,1000);
+  print("new DisplayInterval:",DisplayInterval);
+}
+
+
+//======== BEGIN =======
 g.clear();
 ///Bangle.on('GPS', onGPS);
 ///Bangle.setGPSPower(1, "app");
-//setInterval(onInterval,1000);
+DisplayInterval = setInterval(onInterval,1000);
+
+WatchButton = setWatch(onWatchButton, BTN, {edge:"falling", debounce:100, repeat:false});
 
 //// -------- Testing ---------
-
+/*
 onGPS({fix:1,satellites:3,speed:37.5, 
        time:Date.parse("2021-10-20T14:48:00")}); // testing
 onGPS({fix:1,satellites:3,speed:37.4,
@@ -228,5 +328,5 @@ onGPS({fix:1,satellites:3,speed:35.4,
        time:Date.parse("2021-10-20T14:48:11")}); // testing
 
 onInterval();
-
+*/
 
