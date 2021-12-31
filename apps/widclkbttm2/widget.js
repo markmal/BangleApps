19d1da4331c2754188accf09f4a2f542633a1afc
markmal <markmal@github.com>
2021-12-31 15:52:01 -0500

    updated widclkbttm2

--- apps/widclkbttm2/widget.js
+++ apps/widclkbttm2/widget.js
@@ -1,26 +1,33 @@
 //WIDGETS = {}; // <-- for development only
-  
+
 (function() {
   // don't show widget if we know we have a clock app running
   if (Bangle.CLOCK) return;
 
   let intervalRef = null;
-  var width = 5 * 6*2;
+  var width = 8*12;
   //var text_color=0x07FF;//cyan 
-
+  var inDrawWidgets = false;
+  
   function draw() {
+    if(inDrawWidgets) return;
     g.reset();
-    g.setFont("6x8",3);
+    //g.setFont("6x15",1);
+    //g.setFont("12x20",1);
+    g.setFont("Vector",21);
     g.setFontAlign(-1, 0);//.setColor(text_color);
     //var time = require("locale").time(new Date(),1);
-    var d = (new Date).toString();
+    var d = (new Date()).toString();
     var time = d.substr(d.length-17,8);
-    var w = g.stringWidth(time);
+    var w = g.stringWidth("00:00:00");
     if(WIDGETS.wdclkbttm2.width != w) {
       WIDGETS.wdclkbttm2.width = w;
+      inDrawWidgets = true;
       Bangle.drawWidgets();
+      inDrawWidgets = false;
     }
-    g.drawString(time, this.x, this.y+11, true); // 5 * 6*2 = 60
+    g.clearRect(this.x, this.y, this.x+w, this.y+24);
+    g.drawString(time, this.x, this.y+14, false); // 5 * 6*2 = 60
   }
   function clearTimers(){
     if(intervalRef) {
@@ -46,7 +53,3 @@
 })();
 
 //Bangle.drawWidgets(); // <-- for development only
-
-
-
-
