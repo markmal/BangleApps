7accbd2e6a78534d02f731e2897f14f9f6138d25
markmal <markmal@github.com>
2022-01-25 22:02:54 -0500

    fix nerdic-settings.js

--- apps/nerdic/nerdic-settings.js
+++ apps/nerdic/nerdic-settings.js
@@ -1,3 +1,6 @@
+const RED="#FF0000",GREEN="#00FF00",BLUE="#0000FF",GRAY="#101010",WHITE="#FFFFFF";
+var BangleVer = (g.getWidth()==240)?1:(SW==176)?2:0;
+
 (function(back) {
   var FILE = "nerdic.json";
   // Load settings
@@ -11,18 +14,56 @@
   function writeSettings() {
     require('Storage').writeJSON(FILE, settings);
   }
-
-  // Show the menu
-  E.showMenu({
+  
+  function indexToColor(idx){
+    return ((idx&4)?"#FF":"#00")
+      +((idx&2)?"FF":"00")
+      +((idx&1)?"FF":"00");
+  }
+  
+  function drawColorItem(idx,r){
+    var col=indexToColor(idx);
+    //print(idx,col);
+    g.reset();
+    g.setBgColor("#fff").clearRect(r.x,r.y,r.x+r.w-1,r.y+r.h-1);
+    g.setColor("#000").setFont("6x8:2").drawString(col,r.x+10,r.y+6);
+    g.setColor(col).fillRect(r.x+100,r.y,r.x+r.w-1,r.y+r.h-1);
+  }
+  
+ var nerdicMenu = {
     "" : { "title" : "Nerdic" },
     "< Back" : () => back(),
-    'Hours Color': {
-      value: "#FFFFFF"|settings.hours_color,  // 0| converts undefined to 0
-      min: "#000000", max: "#FFFFFF",
-      onchange: v => {
-        settings.hours_color = v;
+    'Hours Color':  () => E.showScroller({
+      h:32, c:8,
+      draw : (idx, r) => drawColorItem(idx,r),
+      select : (idx) => {
+        settings.hours_color = indexToColor(idx);
         writeSettings();
+        E.showScroller();
+        E.showMenu(nerdicMenu);
       }
+    }),
+    "Minutes Color" : () => E.showScroller({
+      h:32, c:8,
+      draw : (idx, r) => drawColorItem(idx,r),
+      select : (idx) => {
+        settings.minutes_color = indexToColor(idx);
+        writeSettings();
+        E.showScroller();
+        E.showMenu(nerdicMenu);
+      }
+    }),
+    "Seconds Color" : () => E.showScroller({
+      h:32, c:8,
+      draw : (idx, r) => drawColorItem(idx,r),
+      select : (idx) => {
+        settings.seconds_color = indexToColor(idx);
+        writeSettings();
+        E.showScroller();
+        E.showMenu(nerdicMenu);
+      }
+    }),
+
     'Hint Duration': {
       value: 5|settings.hint_duration,  // 0| converts undefined to 0
       min: 0, max: 10,
@@ -30,6 +71,8 @@
         settings.hint_duration = v;
         writeSettings();
       }
-    },
-  });
-})(load);
+    }
+  };
+ // Show the menu
+  E.showMenu(nerdicMenu);
+})(load)
